d38a49cf84c1d5d4a6da43afe4e1b0a957bc1e2d
a7xsuri <leonardofilipposurico@gmail.com>
2022-06-09 17:57:22 +0200

    finish

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hello world!</h1>
+    <a href="{{route('part1')}}">pagina1</a>
+    <a href="{{route('part2')}}">pagina2</a>
+    <a href="{{route('part3')}}">pagina3</a>
+    <a href="{{route('part4')}}">pagina4</a>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home');
+});
+Route::get('welcome', function () {
+    return view('welcome');
+})->name('part1');
+Route::get('parte3', function () {
+    return view('parte3');
+})->name('part2');
+Route::get('parte4', function () {
+    return view('parte4');
+})->name('part3');
+Route::get('parte5', function () {
+    return view('parte5');
+})->name('part4');
